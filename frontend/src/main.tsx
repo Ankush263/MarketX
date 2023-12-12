ca747d4d1aa19bbe4402d4e6e5590d212293b9e1
Ankush263 <ankushbanik263@gmail.com>
2023-12-12 21:27:03 +0530

    update

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -9,6 +9,7 @@ import { store } from './redux/store.ts';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Notifications } from '@mantine/notifications';
+import SingleProductPage from './pages/SingleProductPage.tsx';
 
 const queryClient = new QueryClient();
 
@@ -26,6 +27,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 						}}
 					>
 						<Switch>
+							<Route path={'/products/product/:productId'}>
+								<SingleProductPage />
+							</Route>
 							<Route path={'/products'}>
 								<ProductPage />
 							</Route>
