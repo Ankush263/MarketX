166b3e23dcf31b281ad863fd5ea8a0819ec2784f
Ankush263 <ankushbanik263@gmail.com>
2023-12-20 15:53:30 +0530

    fix it🪄🛠️🛠️

--- dashboard/src/components/uploadproducts/components/ImageDropComponent.tsx
+++ dashboard/src/components/uploadproducts/components/ImageDropComponent.tsx
@@ -31,52 +31,50 @@ function ImageDropComponent(props: ImageInterface) {
 	};
 
 	return (
-		<Box>
-			<Dropzone
-				onDrop={handleImageDrop}
-				onReject={(files) => console.log('rejected files', files)}
-				maxSize={3 * 1024 ** 2}
-				accept={IMAGE_MIME_TYPE}
-				{...props}
+		<Dropzone
+			onDrop={handleImageDrop}
+			onReject={(files) => console.log('rejected files', files)}
+			maxSize={3 * 1024 ** 2}
+			accept={IMAGE_MIME_TYPE}
+			{...props}
+		>
+			<Group
+				position="center"
+				spacing="xl"
+				style={{ minHeight: rem(220), pointerEvents: 'none' }}
 			>
-				<Group
-					position="center"
-					spacing="xl"
-					style={{ minHeight: rem(220), pointerEvents: 'none' }}
-				>
-					<Dropzone.Accept>
-						<IconUpload
-							size="3.2rem"
-							stroke={1.5}
-							color={
-								theme.colors[theme.primaryColor][
-									theme.colorScheme === 'dark' ? 4 : 6
-								]
-							}
-						/>
-					</Dropzone.Accept>
-					<Dropzone.Reject>
-						<IconX
-							size="3.2rem"
-							stroke={1.5}
-							color={theme.colors.red[theme.colorScheme === 'dark' ? 4 : 6]}
-						/>
-					</Dropzone.Reject>
-					<Dropzone.Idle>
-						<IconPhoto size="3.2rem" stroke={1.5} />
-					</Dropzone.Idle>
+				<Dropzone.Accept>
+					<IconUpload
+						size="3.2rem"
+						stroke={1.5}
+						color={
+							theme.colors[theme.primaryColor][
+								theme.colorScheme === 'dark' ? 4 : 6
+							]
+						}
+					/>
+				</Dropzone.Accept>
+				<Dropzone.Reject>
+					<IconX
+						size="3.2rem"
+						stroke={1.5}
+						color={theme.colors.red[theme.colorScheme === 'dark' ? 4 : 6]}
+					/>
+				</Dropzone.Reject>
+				<Dropzone.Idle>
+					<IconPhoto size="3.2rem" stroke={1.5} />
+				</Dropzone.Idle>
 
-					<Box w={500}>
-						<Text size="xl" inline>
-							Drag images here or click to select files
-						</Text>
-						<Text size="sm" color="dimmed" inline mt={7}>
-							Attach only one file, each file should not exceed 5mb
-						</Text>
-					</Box>
-				</Group>
-			</Dropzone>
-		</Box>
+				<Box w={500}>
+					<Text size="xl" inline>
+						Drag images here or click to select files
+					</Text>
+					<Text size="sm" color="dimmed" inline mt={7}>
+						Attach only one file, each file should not exceed 5mb
+					</Text>
+				</Box>
+			</Group>
+		</Dropzone>
 	);
 }
 
